854c6c59f0f35380afe2887c788719b847dcdf41
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-09-08 12:27:26 +0200

    login functional

---
+++ controllers/User.controller.js
@@ -0,0 +1,73 @@
+const User = require('../models/User');
+
+
+module.exports.createUser = async (req, res, next) => {
+    try {
+        const { body } = req;
+        const createdUser = await new User(body);
+        const result = await createdUser.save();
+        res.status(201).send(result);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.findAll();
+        res.send(users);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.getOneUser = async (req, res, next) => {
+    try {
+        const { params: { userId } } = req;
+        const foundedUser = await User.findOne(Number(userId));
+        res.send(foundedUser);
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.deleteUser = async (req, res, next) => {
+    try {
+        const { params: { userId } } = req;
+        const foundedUser = await User.findOne(Number(userId));
+        const result = await foundedUser.delete();
+        res.status(200).send('User deleted');
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports.updateUser = async (req, res, next) => {
+    try {
+        const { body, params: { userId } } = req;
+        const foundedUser = await User.findOne(Number(userId));
+        const result = await foundedUser.update(body);
+        res.send(result);
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        const { body, params: { userId } } = req;
+        const foundedUser = await User.findOne(Number(userId));
+        if (foundedUser.email === body.email) {
+            if (foundedUser.password === body.password) {
+                res.status(202).send('Authorized succesfull');
+            } else {
+                res.status(401).send('Password incorrect');
+            }
+        } else {
+            res.status(403).send('Incorrect data');
+        }
+    } catch (error) {
+        next(error);
+    }
+}
--- index.js
+++ index.js
@@ -1,40 +1,35 @@
 const express = require('express');
 const http = require('http');
-const yup = require('yup');
+const {validateUser} = require('./mw/validations.mw');
+const UserController = require('./controllers/User.controller');
+const {createUser, getAllUsers, getOneUser, deleteUser, updateUser, loginUser} = UserController;
 const bodyParser = express.json();
 const app = express();
+const {ValidationError} = require('yup');
 const PORT = 3000;
 const server = http.createServer(app);
 
-const USER_SCHEMA = yup.object({
-    firstName: yup.string().required(),
-    lastName: yup.string().required(),
-    email: yup.string().required().email(),
-    password: yup.string().required(),
-    isSubscribed: yup.boolean()
-})
 
-const db = [];
+app.post('/users', bodyParser, validateUser , createUser);
+app.post('/users/:userId', bodyParser, loginUser);
+app.get('/users', getAllUsers);
+app.get('/users/:userId', getOneUser);
+app.delete('/users/:userId', deleteUser);
+app.put('/users/:userId',bodyParser, updateUser);
 
-app.post('/user', bodyParser, async (req, res, next) => {
-    const { body } = req;
-    try {
-        const result = await USER_SCHEMA.validate(body);
-        next();
-    } catch (error) {
-        res.status(400).send(error.message);
+const errorHandler = async (err, req, res, next) => {
+    if (err instanceof TypeError) {
+        return res.status(400).send('Invalid request');
     }
-}, (req, res, next) => {
-    const {body} = req;
-    const user = {...body, id: db.length};
-    db.push(user);
-    delete user.password;
-    res.status(201).send(user);
-});
 
-app.get('/user', (req, res, next) => {
-    res.send(db);
-})
+    if (err instanceof ValidationError) {
+        return res.status(401).send(err.message);
+    }
+
+    res.status(404).send();
+}
+
+app.use(errorHandler);
 
 
 server.listen(PORT, () => {
